71bf3aae2129a39a8d8a7fe828c16f6a7dabadd5
victorlun <victor.lundqvist@se.experis.com>
2023-08-16 11:53:18 +0200

    refactor

---
+++ src/test/java/com/film_api/FilmApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.film_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
