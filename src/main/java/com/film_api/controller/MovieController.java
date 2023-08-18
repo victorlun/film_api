b898a488d7b8c0614f95c2fb582055f10bd0df96
victorlun <victor.lundqvist@se.experis.com>
2023-08-18 13:43:26 +0200

    feat: MovieController implemented and  aligned with MovieService

--- src/main/java/com/film_api/controller/MovieController.java
+++ src/main/java/com/film_api/controller/MovieController.java
@@ -1,31 +1,52 @@
 package com.film_api.controller;
 
+import com.film_api.model.Character;
+import com.film_api.model.Movie;
+import com.film_api.service.MovieService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/movies")
 public class MovieController {
+    private final MovieService movieService;
+
+    @Autowired
+    public MovieController(MovieService movieService){
+        this.movieService = movieService;
+    }
 
     @Operation(summary = "Get all movies")
     @GetMapping
-    public ResponseEntity<String> getAll(){
-        return ResponseEntity.ok("Testing get all movies api");
+    public ResponseEntity<List<Movie>> getAll(){
+        List<Movie> movies = movieService.getAllMovies();
+        return new ResponseEntity<>(movies, HttpStatus.OK);
     }
 
     @Operation(summary = "Get specific movie")
     @GetMapping("{id}")
-    public ResponseEntity<String> getById(@PathVariable int id){
-        return ResponseEntity.ok("Testing to get a specific movie with the id of "+ id);
+    public ResponseEntity<Movie> getSpecificMovie(@PathVariable Long id){
+        return movieService.getMovieById(id)
+                .map(movie -> new ResponseEntity<>(movie, HttpStatus.OK))
+                .orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
     @Operation(summary = "Post a movie")
     @PostMapping
-    public ResponseEntity<String> postMovie(){
-        return ResponseEntity.ok("Testing to Post a movie");
+    public ResponseEntity<Movie> postMovie(@RequestBody Movie movie){
+        try {
+            Movie newMovie = movieService.createMovie(movie);
+            return new ResponseEntity<>(newMovie, HttpStatus.CREATED);
+        } catch (Exception e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
     @Operation(summary = "Delete a movie")
@@ -35,8 +56,15 @@ public class MovieController {
             @ApiResponse(responseCode = "404", description = "No movie found with that id")
     })
     @DeleteMapping("{id}")
-    public ResponseEntity<String> delete (@PathVariable int id){
-        //return ResponseEntity.noContent().build();
-            return ResponseEntity.ok("Testing deleting movie api of id "+id);
+    public ResponseEntity<String> delete (@PathVariable Long id){
+        Movie movie = getSpecificMovie(id).getBody();
+        String title = movie.getTitle();
+        try {
+
+            movieService.deleteMovie(id);
+            return new ResponseEntity<>(title +" deleted successfully", HttpStatus.OK);
+        } catch (RuntimeException e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+        }
     }
 }
