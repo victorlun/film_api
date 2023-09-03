4f13cb3f34aaeda49e49eddc93b79af2c272397a
victorlun <victor.lundqvist@se.experis.com>
2023-09-03 22:54:49 +0200

    fix: ApiResponses documentation added

--- src/main/java/com/film_api/controllers/MovieController.java
+++ src/main/java/com/film_api/controllers/MovieController.java
@@ -4,8 +4,8 @@ import com.film_api.models.dtos.moviecharacter.MovieCharacterDTO;
 import com.film_api.models.dtos.movie.MovieDTO;
 import com.film_api.models.entities.Movie;
 import com.film_api.models.dtos.movie.MoviePostDTO;
-import com.film_api.services.CharacterService;
-import com.film_api.services.MovieService;
+import com.film_api.services.character.CharacterServiceImpl;
+import com.film_api.services.movie.MovieService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
@@ -20,18 +20,19 @@ import java.util.List;
 @RequestMapping(path = "api/v1/movies")
 public class MovieController {
     private final MovieService movieService;
-    private final CharacterService characterService;
+    private final CharacterServiceImpl characterServiceImpl;
     @Autowired
-    public MovieController(MovieService movieService, CharacterService characterService){
+    public MovieController(MovieService movieService, CharacterServiceImpl characterServiceImpl){
         this.movieService = movieService;
-        this.characterService = characterService;
+        this.characterServiceImpl = characterServiceImpl;
     }
 
     @Operation(summary = "Get all movies")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Movies successfully parsed"),
-            @ApiResponse(responseCode = "400", description = "Bad Request"),
-            @ApiResponse(responseCode = "404", description = "Bad Request")
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved the list of movies"),
+            @ApiResponse(responseCode = "400", description = "Bad Request, possibly due to invalid query parameters"),
+            @ApiResponse(responseCode = "404", description = "Movies not found"),
+            @ApiResponse(responseCode = "500", description = "An unexpected error occurred")
     })
     @GetMapping
     public ResponseEntity<List<MovieDTO>> getAll() {
@@ -40,6 +41,12 @@ public class MovieController {
     }
 
     @Operation(summary = "Get specific movie")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved movie details"),
+            @ApiResponse(responseCode = "400", description = "Bad Request, check if the ID is valid"),
+            @ApiResponse(responseCode = "404", description = "Movie not found"),
+            @ApiResponse(responseCode = "500", description = "An unexpected error occurred")
+    })
     @GetMapping("{id}")
     public ResponseEntity<MovieDTO> getSpecificMovie(@PathVariable Long id) {
         return movieService.getMovieById(id)
@@ -48,9 +55,15 @@ public class MovieController {
     }
 
     @Operation(summary = "Get all characters in a movie")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Successfully retrieved list of characters"),
+            @ApiResponse(responseCode = "400", description = "Bad Request, check if movieId is valid"),
+            @ApiResponse(responseCode = "404", description = "Movie not found"),
+            @ApiResponse(responseCode = "500", description = "An unexpected error occurred")
+    })
     @GetMapping("/movies/{movieId}/characters")
     public ResponseEntity<List<MovieCharacterDTO>> getAllCharactersByMovie(@PathVariable Long movieId) {
-        List<MovieCharacterDTO> characters = characterService.getAllCharactersByMovie(movieId);
+        List<MovieCharacterDTO> characters = characterServiceImpl.getAllCharactersByMovie(movieId);
         return new ResponseEntity<>(characters, HttpStatus.OK);
     }
 
@@ -59,7 +72,8 @@ public class MovieController {
     @Operation(summary = "Post a movie")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "Movie successfully created"),
-            @ApiResponse(responseCode = "400", description = "Bad Request")
+            @ApiResponse(responseCode = "400", description = "Bad Request"),
+            @ApiResponse(responseCode = "500", description = "An unexpected error occurred")
     })
     @PostMapping
     public ResponseEntity<Movie> postMovie(@RequestBody MoviePostDTO movieDto) {
@@ -79,6 +93,12 @@ public class MovieController {
     }
 
     @Operation(summary = "Update a movie")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Movie successfully updated"),
+            @ApiResponse(responseCode = "400", description = "Malformed request"),
+            @ApiResponse(responseCode = "404", description = "No movie found with that id"),
+            @ApiResponse(responseCode = "500", description = "An unexpected error occurred")
+    })
     @PutMapping("{id}")
     public ResponseEntity<Movie> updateMovie(@PathVariable Long id, @RequestBody MoviePostDTO movieDto) {
         try {
@@ -91,9 +111,10 @@ public class MovieController {
 
     @Operation(summary = "Delete a movie")
     @ApiResponses(value = {
-            @ApiResponse(responseCode = "204", description = "Movie successfully deleted"),
+            @ApiResponse(responseCode = "200", description = "Movie successfully deleted"),
             @ApiResponse(responseCode = "400", description = "Malformed request"),
-            @ApiResponse(responseCode = "404", description = "No movie found with that id")
+            @ApiResponse(responseCode = "404", description = "No movie found with that id"),
+            @ApiResponse(responseCode = "500", description = "An unexpected error occurred")
     })
     @DeleteMapping("{id}")
     public ResponseEntity<String> delete(@PathVariable Long id) {
@@ -108,6 +129,12 @@ public class MovieController {
     }
 
     @Operation(summary = "Update the list of characters that appear in a given movie.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Updated successfully, no content to return"),
+            @ApiResponse(responseCode = "400", description = "Bad request, please follow the API documentation for the proper request format"),
+            @ApiResponse(responseCode = "404", description = "Not found, the specified movie or characters could not be found"),
+            @ApiResponse(responseCode = "500", description = "An unexpected error occurred")
+    })
     @PutMapping("{id}/characters")
     public ResponseEntity<Void> updateCharacters(@PathVariable Long id, @RequestBody int[] charactersIds) {
         movieService.updateCharacters(id, charactersIds);
